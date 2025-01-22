b87dbc88e07eec841811a11e8d0dbb39d89b6132
Charity <cford24@uco.edu>
2025-01-22 16:13:10 -0600

    changed font color

--- AppCanvas.java
+++ AppCanvas.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -13,6 +15,10 @@ public class AppCanvas extends JPanel {
     
     @Override
     public void paintComponent(Graphics g){
+        var font = new Font("Courier New",Font.BOLD,24);
+        g.setFont(font);
+
+        g.setColor(Color.BLUE);
         g.drawString("Lucky Number Is:", 30, 100);
         g.drawString(""+ randomNumber,30,140);
     }
